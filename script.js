0fbc2aa57c3fc2d5f9587fa0fe2a40d2e89ccda1
Fausto González Castro <fau.gc90@gmail.com>
2024-12-05 23:18:04 -0300

    Actualizado al 05/12/2024

--- script.js
+++ script.js
@@ -1,73 +1,77 @@
-const productos =[
-    {id:1, nombre: "Habitación grand", categoria: "habitaciones", subcategoria:"habitaciones", stock: 10, precio: 160, pasajeros:6, imagen:"grand.jpg" },
-    {id:2, nombre: "Habitación quintuple", categoria: "habitaciones", subcategoria:"habitaciones", stock: 10, precio: 140, pasajeros:5, imagen:"quintuple.jpg" },
-    {id:3, nombre: "Habitación cuadruple", categoria: "habitaciones", subcategoria:"habitaciones", stock: 10, precio: 120, pasajeros:4, imagen:"cuadruple.jpg" },
-    {id:4, nombre: "Habitación triple", categoria: "habitaciones", subcategoria:"habitaciones", stock: 10, precio: 100, pasajeros:3, imagen:"triple.jpg" },
-    {id:5, nombre: "Habitación doble", categoria: "habitaciones", subcategoria:"habitaciones", stock: 10, precio: 80, pasajeros:2, imagen:"doble.jpg" },
+// Función para obtener la imagen del producto (si no existe, puedes usar una imagen predeterminada)
+function obtenerImagen(nombre) {
+    const productos = JSON.parse(localStorage.getItem('productos')) || [];  // Definir productos globalmente
+    const producto = productos.find(item => item.nombre === nombre);
+    return producto ? producto.imagen : 'comingsoon.jpeg';
+}
 
-    {id:6, nombre: "Café", categoria: "servicio-habitacion", subcategoria:"bebida - infusión", stock: 1000, precio: 4, pasajeros:null, imagen:"cafe.jpg" },
-    {id:7, nombre: "Té", categoria: "servicio-habitacion", subcategoria:"bebida - infusión", stock: 1000, precio: 4, pasajeros:null, imagen:"te.jpeg" },
-    {id:8, nombre: "Mate cocido", categoria: "servicio-habitacion", subcategoria:"bebida - infusión", stock: 1000, precio: 3, pasajeros:null, imagen:"matecocido.jpg" },
-    {id:9, nombre: "Medialunas", categoria: "servicio-habitacion", subcategoria:"alimento - panificado", stock: 2000, precio: 2, pasajeros:null, imagen:"medialunas.jpg"},
-    {id:10, nombre: "Tostadas", categoria: "servicio-habitacion", subcategoria:"alimento - panificado", stock: 2000, precio: 2, pasajeros:null, imagen:"tostadas.jpg"},
-    {id:11, nombre: "Milanesas", categoria: "servicio-habitacion", subcategoria:"alimento - carne", stock: 2000, precio: 5, pasajeros:null, imagen:"milanesas.png"},
-    {id:12, nombre: "Asado de banderitas", categoria: "servicio-habitacion", subcategoria:"alimento - carne", stock: 2000, precio: 8, pasajeros:null, imagen:"asadobanderita.PNG"},
-    {id:13, nombre: "Pure", categoria: "servicio-habitacion", subcategoria:"alimento - guarnición", stock: 2000, precio: 4, pasajeros:null, imagen:"pure.jpg"},
-    {id:14, nombre: "Ensalada", categoria: "servicio-habitacion", subcategoria:"alimento - guarnición", stock: 2000, precio: 4, pasajeros:null, imagen:"ensalada.jpg"},
-    {id:15, nombre: "Papas fritas", categoria: "servicio-habitacion", subcategoria:"alimento - guarnición", stock: 2000, precio: 2, pasajeros:null, imagen:"papasfritas.jpg"},
-    {id:16, nombre: "Caipirinha", categoria: "servicio-habitacion", subcategoria:"bebida alcoholica", stock: 1000, precio: 5, pasajeros:null, imagen:"caipirinha.PNG"},
-    {id:17, nombre: "Caipiroska", categoria: "servicio-habitacion", subcategoria:"bebida alcoholica", stock: 1000, precio: 5, pasajeros:null, imagen:"caipiroska.jpg"},
-    {id:18, nombre: "Fernet-cola", categoria: "servicio-habitacion", subcategoria:"bebida alcoholica", stock: 1000, precio: 5, pasajeros:null, imagen:"fernetcola.png"},
-    {id:19, nombre: "Gancia-limón", categoria: "servicio-habitacion", subcategoria:"bebida alcoholica", stock: 1000, precio: 5, pasajeros:null, imagen:"gancia-limon.jpg"},
-    {id:20, nombre: "Cerveza", categoria: "servicio-habitacion", subcategoria:"bebida alcoholica", stock: 2000, precio: 3, pasajeros:null, imagen:"cerveza.PNG"},
-    {id:21, nombre: "Agua", categoria: "servicio-habitacion", subcategoria:"bebida sin alcohol", stock: 2000, precio: 1, pasajeros:null, imagen:"agua.PNG"},
-    {id:22, nombre: "Caipirinha sin alcohol", categoria: "servicio-habitacion", subcategoria:"bebida sin alcohol", stock: 2000, precio: 4, pasajeros:null, imagen:"caipirinha.PNG"},
-    {id:23, nombre: "Caipiroska sin alcohol", categoria: "servicio-habitacion", subcategoria:"bebida sin alcohol", stock: 2000, precio: 4, pasajeros:null, imagen:"caipiroska.jpg"},
-    {id:24, nombre: "Licuado", categoria: "servicio-habitacion", subcategoria:"bebida sin alcohol", stock: 2000, precio: 2, pasajeros:null, imagen:"licuado.jpg"},
-    {id:25, nombre: "Jugo de frutas", categoria: "servicio-habitacion", subcategoria:"bebida sin alcohol", stock: 2000, precio: 2, pasajeros:null, imagen:"juegodefrutas.jpg"},
-    {id:26, nombre: "Gaseosa", categoria: "servicio-habitacion", subcategoria:"bebida sin alcohol", stock: 2000, precio: 3, pasajeros:null, imagen:"gaseosa.PNG"},
-    
-    
-    {id:27, nombre: "Café", categoria: "servicio-playa", subcategoria:"bebida - infusión", stock: 1000, precio: 5, pasajeros:null, imagen:"cafe.jpg" },
-    {id:28, nombre: "Té", categoria: "servicio-playa", subcategoria:"bebida - infusión", stock: 1000, precio: 5, pasajeros:null, imagen:"te.jpeg" },
-    {id:29, nombre: "Mate cocido", categoria: "servicio-playa", subcategoria:"bebida - infusión", stock: 1000, precio: 4, pasajeros:null, imagen:"matecocido.jpg" },
-    {id:30, nombre: "Medialunas", categoria: "servicio-playa", subcategoria:"alimento - panificado", stock: 2000, precio: 3, pasajeros:null, imagen:"medialunas.jpg"},
-    {id:31, nombre: "Tostadas", categoria: "servicio-playa", subcategoria:"alimento - panificado", stock: 2000, precio: 3, pasajeros:null, imagen:"tostadas.jpg"},
-    {id:32, nombre: "Milanesas", categoria: "servicio-playa", subcategoria:"alimento - carne", stock: 2000, precio: 6, pasajeros:null, imagen:"milanesas.png"},
-    {id:33, nombre: "Asado de banderitas", categoria: "servicio-playa", subcategoria:"alimento - carne", stock: 2000, precio: 8, pasajeros:null, imagen:"asadobanderita.PNG"},
-    {id:34, nombre: "Pure", categoria: "servicio-playa", subcategoria:"alimento - guarnición", stock: 2000, precio: 5, pasajeros:null, imagen:"pure.jpg"},
-    {id:35, nombre: "Ensalada", categoria: "servicio-playa", subcategoria:"alimento - guarnición", stock: 2000, precio: 5, pasajeros:null, imagen:"ensalada.jpg"},
-    {id:36, nombre: "Papas fritas", categoria: "servicio-playa", subcategoria:"alimento - guarnición", stock: 2000, precio: 3, pasajeros:null, imagen:"papasfritas.jpg"},
-    {id:37, nombre: "Caipirinha", categoria: "servicio-playa", subcategoria:"bebida alcoholica", stock: 1000, precio: 6, pasajeros:null, imagen:"caipirinha.PNG"},
-    {id:38, nombre: "Caipiroska", categoria: "servicio-playa", subcategoria:"bebida alcoholica", stock: 1000, precio: 6, pasajeros:null, imagen:"caipiroska.jpg"},
-    {id:39, nombre: "Fernet-cola", categoria: "servicio-playa", subcategoria:"bebida alcoholica", stock: 1000, precio: 6, pasajeros:null, imagen:"fernetcola.png"},
-    {id:40, nombre: "Gancia-limón", categoria: "servicio-playa", subcategoria:"bebida alcoholica", stock: 1000, precio: 6, pasajeros:null, imagen:"gancia-limon.jpg"},
-    {id:41, nombre: "Cerveza", categoria: "servicio-playa", subcategoria:"bebida alcoholica", stock: 2000, precio: 4, pasajeros:null, imagen:"cerveza.PNG"},
-    {id:42, nombre: "Agua", categoria: "servicio-playa", subcategoria:"bebida sin alcohol", stock: 2000, precio: 2, pasajeros:null, imagen:"agua.PNG"},
-    {id:43, nombre: "Caipirinha sin alcohol", categoria: "servicio-playa", subcategoria:"bebida sin alcohol", stock: 2000, precio: 5, pasajeros:null, imagen:"caipirinha.PNG"},
-    {id:44, nombre: "Caipiroska sin alcohol", categoria: "servicio-playa", subcategoria:"bebida sin alcohol", stock: 2000, precio: 5, pasajeros:null , imagen:"caipiroska.jpg"},
-    {id:45, nombre: "Licuado", categoria: "servicio-playa", subcategoria:"bebida sin alcohol", stock: 2000, precio: 3, pasajeros:null , imagen:"licuado.jpg"},
-    {id:46, nombre: "Jugo de frutas", categoria: "servicio-playa", subcategoria:"bebida sin alcohol", stock: 2000, precio: 3, pasajeros:null , imagen:"juegodefrutas.jpg"},
-    {id:47, nombre: "Gaseosa", categoria: "servicio-playa", subcategoria:"bebida sin alcohol", stock: 2000, precio: 3, pasajeros:null, imagen:"gaseosa.PNG" },
-    {id:48, nombre: "Alquiler de reposera", categoria: "servicio-playa", subcategoria:"bebida sin alcohol", stock: 2000, precio: 2, pasajeros:null, imagen:"reposera.PNG" },
-    {id:49, nombre: "Alquiler de mesa de playa", categoria: "servicio-playa", subcategoria:"bebida sin alcohol", stock: 2000, precio: 2, pasajeros:null , imagen:"mesitadeplaya.PNG" },
-    {id:50, nombre: "Alquiler de sombrilla", categoria: "servicio-playa", subcategoria:"bebida sin alcohol", stock: 2000, precio: 2, pasajeros:null , imagen:"sombrilla.PNG" },
+// Cargar los productos al cargar la página
+window.onload = function() {
+    mostrarFormularioFechas();
+};
 
-];
+// Función para mostrar el formulario de fechas
+function mostrarFormularioFechas() {
+    const contenedor = document.querySelector('.contenedor-fechas');
 
-// Función para agregar los productos dinámicamente
-function agregarProductos() {
-    // Obtener los contenedores donde se agregarán los productos
+    // Recuperar las fechas guardadas en localStorage, si existen
+    const fechaLlegada = localStorage.getItem('fechaLlegada') || '';
+    const fechaSalida = localStorage.getItem('fechaSalida') || '';
+
+    contenedor.innerHTML = `
+        <h2>Por favor, ingresa las fechas de llegada y salida:</h2>
+        <label for="fecha-llegada">Fecha de Llegada:</label>
+        <input type="date" id="fecha-llegada" value="${fechaLlegada}">
+        <label for="fecha-salida">Fecha de Salida:</label>
+        <input type="date" id="fecha-salida" value="${fechaSalida}">
+        <button id="confirmar-fechas">Seleccionar fechas</button>
+    `;
+
+    // Asignar evento al botón de actualizar fechas
+    document.getElementById('confirmar-fechas').addEventListener('click', function() {
+        const fechaLlegada = document.getElementById('fecha-llegada').value;
+        const fechaSalida = document.getElementById('fecha-salida').value;
+
+        // Verificar que ambas fechas sean seleccionadas
+        if (fechaLlegada && fechaSalida) {
+            // Guardar fechas en el localStorage
+            localStorage.setItem('fechaLlegada', fechaLlegada);
+            localStorage.setItem('fechaSalida', fechaSalida);
+            mostrarProductos();
+        } else {
+            alert("Por favor, completa ambas fechas.");
+        }
+    });
+}
+
+// Función para mostrar los productos
+function mostrarProductos() {
+    fetch('./info.json')
+        .then(response => response.json())
+        .then(productos => {
+            // Guardamos los productos en el localStorage
+            localStorage.setItem('productos', JSON.stringify(productos));
+            agregarProductos(productos);
+        })
+        .catch(error => console.error("Error al cargar los productos:", error));
+}
+
+// Función para agregar los productos al DOM
+function agregarProductos(productos) {
     const contenedorHabitaciones = document.querySelector('.productos-habitaciones');
     const contenedorServiciosHabitacion = document.querySelector('.productos-servicios-habitacion');
     const contenedorServiciosPlaya = document.querySelector('.productos-servicios-playa');
 
-    // Recorrer el array de productos y agregar las tarjetas en los contenedores correspondientes
+    if (!contenedorHabitaciones || !contenedorServiciosHabitacion || !contenedorServiciosPlaya) {
+        console.error("No se encontraron los contenedores en el HTML");
+        return;
+    }
+
     productos.forEach(producto => {
         const tarjeta = document.createElement('div');
-        tarjeta.classList.add('catalogo'); 
+        tarjeta.classList.add('catalogo');
 
-        tarjeta.innerHTML = `            
+        tarjeta.innerHTML = `
             <h3>${producto.nombre}</h3>
             <img src="./imagenes/${producto.imagen}" alt="${producto.nombre}" class="imagen-producto">
             <p class="precio">$${producto.precio}</p>
@@ -76,7 +80,6 @@ function agregarProductos() {
             <button>Agregar al Carrito</button>
         `;
 
-        // Dependiendo de la categoría, agregar el producto en el contenedor correspondiente
         if (producto.categoria === "habitaciones") {
             contenedorHabitaciones.appendChild(tarjeta);
         } else if (producto.categoria === "servicio-habitacion") {
@@ -115,6 +118,17 @@ function agregarAlCarrito(nombre, precio, cantidad) {
 
     // Guardar el carrito actualizado en el localStorage
     localStorage.setItem('carrito', JSON.stringify(carrito));
+
+    // Mostrar mensaje en la parte inferior derecha de la pantalla
+    const mensaje = document.createElement('div');
+    mensaje.classList.add('mensaje-carrito');
+    mensaje.innerHTML = `${nombre} agregado al carrito`;
+
+    // Agregar el mensaje al body y luego eliminarlo después de 0.7 segundos
+    document.body.appendChild(mensaje);
+    setTimeout(() => {
+        mensaje.remove();
+    }, 1000);
 
     // Actualizar la visualización del carrito
     mostrarCarrito();
@@ -180,7 +194,7 @@ function mostrarCarrito() {
 
     // Crear un contenedor para los productos del carrito
     const productosCarrito = document.createElement('div');
-    productosCarrito.classList.add('productos-carrito'); // Añadir clase para flexbox
+    productosCarrito.classList.add('productos-carrito');
 
     carrito.forEach(item => {
         total += item.precio * item.cantidad;
@@ -196,9 +210,7 @@ function mostrarCarrito() {
                         <button class="sumar" data-nombre="${item.nombre}">+</button>
                         <button class="restar" data-nombre="${item.nombre}">-</button>
                         <button class="eliminar" data-nombre="${item.nombre}">Eliminar</button>
-                        
                     </div>
-                    
                 </div>
             </div>
         `;
@@ -218,8 +230,6 @@ function mostrarCarrito() {
         </div>
     `;
     carritoContainer.appendChild(totalCarrito);
-    
-
 
     // Asignar eventos para sumar, restar y eliminar productos
     const botonesRestar = document.querySelectorAll('.restar');
@@ -245,16 +255,23 @@ function mostrarCarrito() {
             eliminarProducto(nombreProducto);
         });
     });
+
+    // Evento para el botón de enviar consulta
+    const botonEnviarConsulta = document.querySelector('.btn-enviar-consulta');
+    if (botonEnviarConsulta) {
+        botonEnviarConsulta.addEventListener('click', function() {
+            // Mostrar mensaje con SweetAlert2 al enviar la consulta
+            Swal.fire({
+                icon: 'success',
+                title: 'Solicitud enviada',
+                text: 'Nos estaremos poniendo en contacto contigo en la brevedad. Nos vemos Pronto!',
+                confirmButtonText: '¡Gracias!'
+            });
+        });
+    }
 }
 
-// Función para obtener la imagen del producto (si no existe, puedes usar una imagen predeterminada)
-function obtenerImagen(nombre) {
-    const producto = productos.find(item => item.nombre === nombre);
-    return producto ? producto.imagen : 'comingsoon.jpeg';
-}
-
-// Asignar el evento para el botón "Ver carrito"
-document.querySelector('.ver-carrito').addEventListener('click', mostrarCarrito);
-
-// Cargar los productos al cargar la página
-window.onload = agregarProductos;
+// Asignar el botón para ver el carrito
+document.getElementById('ver-carrito-btn').addEventListener('click', function() {
+    document.getElementById('carrito-container').classList.toggle('visible');
+});
